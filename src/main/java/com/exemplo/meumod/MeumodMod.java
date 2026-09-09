2b19ec62da87c9cca8985c600c8534155ace2287
tosh.GG <marlonesquerdo0@gmail.com>
2026-09-09 15:12:37 -0300

    chore: atualizar src/main/java/com/exemplo/meumod/MeumodMod.java

--- src/main/java/com/exemplo/meumod/MeumodMod.java
+++ src/main/java/com/exemplo/meumod/MeumodMod.java
@@ -10,6 +10,40 @@ public class MeumodMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Meu Mod carregado!");
+        package com.autobuilder;
+
+        import net.fabricmc.api.ClientModInitializer;
+        import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+        import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+        import net.minecraft.client.option.KeyBinding;
+        import net.minecraft.client.util.InputUtil;
+        import org.lwjgl.glfw.GLFW;
+
+        public class AutobuilderMod implements ClientModInitializer {
+
+    public static KeyBinding toggleKey;
+    public static BuildController controller;
+
+    @Override
+    public void onInitializeClient() {
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.autobuilder.toggle",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_B,
+            "category.autobuilder"
+        ));
+
+        controller = new BuildController();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (toggleKey.wasPressed()) {
+                controller.toggle(client);
+            }
+            if (controller.isRunning()) {
+                controller.tick(client);
+            }
+        });
+    }
+        }
     }
 }
